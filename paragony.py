1a165522a8ca77de7dd19d1457f1a1dbd9e5174c
Iryna Skachkova <i.skachkova.aa@gmail.com>
2025-10-20 18:37:29 +0200

    fixed bug when price_gr starts with 0

--- paragony.py
+++ paragony.py
@@ -1,10 +1,10 @@
 def get_description(name, price):
     price_zl = price // 100
     price_gr = price % 100
-    return f'Price of {name} is {price_zl}.{price_gr}'
+    return f'Price of {name} is {price_zl}.{price_gr:02}'
 def print_description(name, price): 
     description = (get_description(name, price))
     print(description)
 
-print_description("Avocado", 557)
+# print_description("Avocado", 557)
 print_description("Apples", 200)
